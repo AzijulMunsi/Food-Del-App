93def840c62798d5bc1cf0e79bdc08ef42a2b0dd
AzijulMunsi <azijulmunsi411@gmail.com>
2025-01-02 20:14:26 +0530

    updare

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import StoreContextProvider from './contexts/storeContext.jsx'
+import StoreContextProvider from './contexts/StoreContext.jsx'
 
 ReactDom.createRoot(document.getElementById('root')).render(
 
